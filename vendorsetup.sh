7d3e3103bfeadea1fdb01f974c5505ba1b52f325
PugzAreCute <me@pugzarecute.com>
2023-12-10 16:30:04 +0530

    add proper vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,18 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
+
+git clone https://github.com/redznn/vendor_xiaomi_spes vendor/xiaomi/spes -b 14.0
+cd vendor/xiaomi/spes
+git pull
+cd ../../..
+git clone https://github.com/muralivijay/kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225 -b android-14
+cd kernel/xiaomi/sm6225
+git pull
+cd ../../..
+
+# hardware sources
+git clone https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi -b fourteen
+cd hardware/xiaomi
+git pull
+cd ../..
